90a21ede14d0ba100f3d72eb6698aa43e0286f1e
Zimma013 <mikhailkupiec@gmail.com>
2019-12-12 13:53:58 +0100

    Added time ranges for random alert generation modifiers

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -7,14 +7,14 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Application {
-	public static IterationDataCounter iterationDataCounter = new IterationDataCounter();
+
 	public static ExcelWriter excelWriter = new ExcelWriter();
 
 	// Simulation params
-	private static final Double mean = 14.0; // peak time of population in simulation
-	private static final Double standardDeviation = 5.0; // https://homepage.divms.uiowa.edu/~mbognar/applets/normal.html
+	private static final Double mean = 14D; // peak time of population in simulation
+	private static final Double standardDeviation = 5D; // https://homepage.divms.uiowa.edu/~mbognar/applets/normal.html
 	private static final Integer maxPopulation = 500;
-	private static final Double iterationTimeOffset = 0.5; // half hour
+	private static final Double iterationTimeOffset = 0.5D; // half hour
 	private static final Integer iterationCount = new BigDecimal(24).divide(new BigDecimal(iterationTimeOffset), 0, RoundingMode.HALF_UP).intValue();
 
 
@@ -25,8 +25,17 @@ public class Application {
 				.singleIterationTimeOffset(iterationTimeOffset)
 				.startTime(5D)
 				.peakTime(mean)
-//				.endTime(24D + 5D - iterationTimeOffset)
+				.endTime(24D + 5D - iterationTimeOffset)
 				.iterationCount(iterationCount)
+				.timeRangeOneStart(5D)
+				.timeRangeOneEnd(14D)
+				.timeRangeOneModifier(1D)
+				.timeRangeTwoStart(14D)
+				.timeRangeTwoEnd(18D)
+				.timeRangeTwoModifier(1.3D)
+				.timeRangeThreeStart(18D)
+				.timeRangeThreeEnd(5D)
+				.timeRangeThreeModifier(2D)
 				.build();
 
 
--- src/main/java/main/Simulator.java
+++ src/main/java/main/Simulator.java
@@ -3,6 +3,9 @@ package main;
 import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class Simulator {
+
+	public IterationDataCounter iterationDataCounter;
+
 	private NormalDistribution normalDistribution;
 	private int maxPopulation;
 	private Double startTime;
@@ -11,20 +14,35 @@ public class Simulator {
 	private Double singleIterationTimeOffset;
 	private int iterationCount;
 
+	private Double timeRangeOneStart;
+	private Double timeRangeOneEnd;
+	private Double timeRangeTwoStart;
+	private Double timeRangeTwoEnd;
+	private Double timeRangeThreeStart;
+	private Double timeRangeThreeEnd;
+	private Double timeRangeOneModifier;
+	private Double timeRangeTwoModifier;
+	private Double timeRangeThreeModifier;
+
+
 	private void getSampleIteration(int iterationNumber) {
+		iterationDataCounter = new IterationDataCounter();
 		int currentPopulationCount = 0;
-		Double iterationTime = iterationNumber * singleIterationTimeOffset;
+		double iterationTime = iterationNumber * singleIterationTimeOffset + startTime; // should range from startTime to 24 + startTime (29)
 		if (iterationNumber <= (iterationCount/2)) {
 			currentPopulationCount = (int) (normalDistribution.cumulativeProbability(iterationTime) * maxPopulation);
 		} else {
 			currentPopulationCount = (int) (normalDistribution.inverseCumulativeProbability(iterationTime) * maxPopulation);
 		}
 
+		// one iteration is checking one person from population
 		for (int pop = 0; pop < currentPopulationCount; pop++) {
 			// weather alerts
 
 			// situation alerts
 		}
+
+		Application.excelWriter.addToDataList(iterationDataCounter);
 	}
 
 
@@ -36,6 +54,15 @@ public class Simulator {
 		peakTime = builder.peakTime;
 		singleIterationTimeOffset = builder.singleIterationTimeOffset;
 		iterationCount = builder.iterationCount;
+		timeRangeOneStart = builder.timeRangeOneStart;
+		timeRangeOneEnd = builder.timeRangeOneEnd;
+		timeRangeTwoStart = builder.timeRangeTwoStart;
+		timeRangeTwoEnd = builder.timeRangeTwoEnd;
+		timeRangeThreeStart = builder.timeRangeThreeStart;
+		timeRangeThreeEnd = builder.timeRangeThreeEnd;
+		timeRangeOneModifier = builder.timeRangeOneModifier;
+		timeRangeTwoModifier = builder.timeRangeTwoModifier;
+		timeRangeThreeModifier = builder.timeRangeThreeModifier;
 	}
 
 	public static final class Builder {
@@ -46,6 +73,15 @@ public class Simulator {
 		private Double peakTime;
 		private Double singleIterationTimeOffset;
 		private int iterationCount;
+		private Double timeRangeOneStart;
+		private Double timeRangeOneEnd;
+		private Double timeRangeTwoStart;
+		private Double timeRangeTwoEnd;
+		private Double timeRangeThreeStart;
+		private Double timeRangeThreeEnd;
+		private Double timeRangeOneModifier;
+		private Double timeRangeTwoModifier;
+		private Double timeRangeThreeModifier;
 
 		public Builder() {
 		}
@@ -82,6 +118,51 @@ public class Simulator {
 
 		public Builder iterationCount(int val) {
 			iterationCount = val;
+			return this;
+		}
+
+		public Builder timeRangeOneStart(Double val) {
+			timeRangeOneStart = val;
+			return this;
+		}
+
+		public Builder timeRangeOneEnd(Double val) {
+			timeRangeOneEnd = val;
+			return this;
+		}
+
+		public Builder timeRangeTwoStart(Double val) {
+			timeRangeTwoStart = val;
+			return this;
+		}
+
+		public Builder timeRangeTwoEnd(Double val) {
+			timeRangeTwoEnd = val;
+			return this;
+		}
+
+		public Builder timeRangeThreeStart(Double val) {
+			timeRangeThreeStart = val;
+			return this;
+		}
+
+		public Builder timeRangeThreeEnd(Double val) {
+			timeRangeThreeEnd = val;
+			return this;
+		}
+
+		public Builder timeRangeOneModifier(Double val) {
+			timeRangeOneModifier = val;
+			return this;
+		}
+
+		public Builder timeRangeTwoModifier(Double val) {
+			timeRangeTwoModifier = val;
+			return this;
+		}
+
+		public Builder timeRangeThreeModifier(Double val) {
+			timeRangeThreeModifier = val;
 			return this;
 		}
 
